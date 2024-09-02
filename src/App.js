b76fadd96117df71b8971a09e126fbecb3c64879
Shernan Jenesis Mateo <shernanjenesismateo0@yahoo.com>
2024-09-02 13:45:42 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    const displayTime = () => {
+      const now = new Date();
+      const options = { hour12: false, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+      const timeString = now.toLocaleTimeString(undefined, options);
+      setTime(timeString);
+    };
+
+    const intervalId = setInterval(displayTime, 1000); // Update time every second
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <div style={styles.clock}>{time}</div>
     </div>
   );
 }
+
+const styles = {
+  container: {
+    margin: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  clock: {
+    fontSize: '48px',
+    fontFamily: 'sans-serif',
+    border: '10px solid red',
+    padding: '30px',
+    height: '100px',
+    width: '500px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
 
 export default App;
